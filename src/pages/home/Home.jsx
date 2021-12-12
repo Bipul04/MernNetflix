5ea444a99992e9426d210dc051631ac2fa90cd3f
Bipul <broydanuwar4@gmail.com>
2021-12-12 23:02:12 +0545

    all commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import Featured from "../../components/featured/Featured";
+import List from "../../components/list/List";
+import Nabvar from "../../components/navbar/Nabvar";
+import "./home.scss";
+import axios from "axios";
+
+const Home = ({type}) => {
+    const [lists, setLists] = useState([]);
+    const [genre, setGenre] = useState(null);
+
+    useEffect(()=>{
+        const getRandomLists = async ()=>{
+            try{
+                const res=  await axios.get(`lists${type ? "?type=" + type: ""}${genre ? "&genre=" + genre : ""}`, {
+                    headers: {
+                        token: "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken
+                    }
+                });
+                setLists(res.data)
+            }catch(err){
+                console.log(err)
+            }
+        };
+        getRandomLists();
+    },[type, genre])
+    return (
+        <div className="home">
+            <Nabvar />
+            <Featured type={type} setGenre={setGenre} />
+            {lists.map((list) => (
+                <List list={list} />
+            ))}
+        </div>
+    )
+}
+
+export default Home
